e103f360a68bd5774d82f404812a589dff2c069d
William Dale <williambdale@gmail.com>
2018-04-07 14:27:03 -0400

    Create an anagram finder

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,17 @@
-# Your code goes here!
+class Anagram
+
+  attr_accessor :word
+
+  def initialize(word)
+    @word = word
+  end
+
+  def match(words_to_match)
+    words_to_match.select {|new_word| sort_word(new_word) == sort_word(word)}
+  end
+
+  def sort_word(word)
+    word.split('').sort.join('')
+  end
+
+end
