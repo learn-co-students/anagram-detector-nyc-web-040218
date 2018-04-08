80d461813983105fafa456ef7ecbb18c990b0eaa
LaneMiller <Chris.Lane.Miller@gmail.com>
2018-04-08 17:53:56 -0400

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,15 @@
-# Your code goes here!
+require 'pry'
+class Anagram
+  attr_accessor :word
+
+  def initialize(word)
+    @word = word
+  end
+
+  def match(anagrams)
+    anagrams.find_all { |test_word|
+      test_word.split("").sort == word.split("").sort
+    }
+  end
+
+end
