eaecd5964f8f42525a6014d49f81620f1d648b6c
Ryan Hu <ryanhuuu@gmail.com>
2018-04-09 14:43:53 -0400

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,17 @@
 # Your code goes here!
+class Anagram
+
+  def initialize(word)
+    @word = word
+  end
+
+  def match(new_string)
+    sorted = @word.chars.sort.join
+
+    alt_string = (new_string).select do |item|
+      item.chars.sort.join == sorted
+    end
+
+
+  end
+end
